b9e45e17ff3a6282392d7e7463e6e3c09711112c
Gustav <sir.gustav.the.coder@gmail.com>
2016-08-28 09:13:47 +0200

    started lua test

--- selene/selene-test.cc
+++ selene/selene-test.cc
@@ -6,8 +6,17 @@ int main() {
   using namespace sel;
   State state{true};
   if( false == state.Load("test.lua") ) {
-    std::cerr << "Failed to load test\n";
+    std::cerr << "Failed to load test.lua\n";
     return 1;
   }
+
+  // accessing variables
+  const int foo = state["foo"];
+  const std::string bar = state["bar"][3];
+  const std::string key = state["bar"]["key"];
+  std::cout << "foo(4) " << foo << "\n";
+  std::cout << "bar(hi) " << bar << "\n";
+  std::cout << "key(there) " << key << "\n";
+
   return 0;
 }

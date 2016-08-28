b291668d8b096be47cdb8d6c4c721a671f38646d
Gustav <sir.gustav.the.coder@gmail.com>
2016-08-28 12:50:46 +0200

    added more lua selene tests

--- lua-selene/selene-test.cc
+++ lua-selene/selene-test.cc
@@ -2,6 +2,10 @@
 
 #include "selene.h"
 
+void TestFunc() {
+  std::cout << "I'm a function in C++\n";
+}
+
 int main() {
   using namespace sel;
   State state{true};
@@ -10,13 +14,30 @@ int main() {
     return 1;
   }
 
-  // accessing variables
+  std::cout << "\nAccessing variables\n";
   const int foo = state["foo"];
   const std::string bar = state["bar"][3];
   const std::string key = state["bar"]["key"];
-  std::cout << "foo(4) " << foo << "\n";
-  std::cout << "bar(hi) " << bar << "\n";
-  std::cout << "key(there) " << key << "\n";
+  std::cout << "foo(4): " << foo << "\n";
+  std::cout << "bar(hi): " << bar << "\n";
+  std::cout << "key(there): " << key << "\n";
+
+  std::cout << "\nCalling lua functions\n";
+
+  state["foo"]();
+
+  int result = state["add"](5, 2);
+  std::cout << "result(7) " << result << "\n";
+  int sum, difference;
+  sel::tie(sum, difference) = state["sum_and_difference"](3, 1);
+  std::cout << "sum(4): " << sum << "\ndiff(2): " << difference << "\n";
+
+  result = state["mytable"]["foo"]();
+  std::cout << "result(4): " << result << "\n";
+
+  std::cout << "\nCalling C++ function from lua\n";
+  state["test_func"] = &TestFunc;
+  state("test_func()");
 
   return 0;
 }
--- scripts/test.lua
+++ scripts/test.lua
@@ -1,4 +1,26 @@
+-- accessing variables
 foo = 4
 bar = {}
 bar[3] = "hi"
 bar["key"] = "there"
+
+-- calling lua functions
+function foo()
+end
+
+function add(a, b)
+    return a + b
+end
+
+function sum_and_difference(a, b)
+    return (a+b), (a-b);
+end
+
+function bar()
+    return 4, true, "hi"
+end
+
+mytable = {}
+function mytable.foo()
+    return 4
+end

51a885dbada7e7e39595461e950efe2c0880d124
Gustav <sir.gustav.the.coder@gmail.com>
2016-09-01 06:49:00 +0200

    'fixed' luabind issues

--- src/lua_luabind.cc
+++ src/lua_luabind.cc
@@ -8,6 +8,26 @@ extern "C"
 
 #include <luabind/luabind.hpp>
 
+void greet()
+{
+  std::cout << "hello world!\n";
+}
+
+extern "C" int init(lua_State* L)
+{
+  using namespace luabind;
+
+  open(L);
+
+  module(L)
+  [
+      def("greet", &greet)
+  ];
+
+  return 0;
+}
+
+
 int main() {
   std::cout << "Hello world!\n";
   return 0;
